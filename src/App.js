186ea103e95d2c0ad71bcb91b0f8985f4fbd0245
vishnulek <lekshmipriyas2008@gmail.com>
2024-10-17 23:24:43 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Viewall from './components/Products';
 import Products from './components/Products';
+import Add from './components/Add';
+import Delete from './components/Delete';
+import Search from './components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      <Products/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Add/>}/>
+    <Route path='/search' element={<Search/>}/>
+    <Route path='/delete' element={<Delete/>}/>
+    <Route path='/viewall' element={<Products/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
